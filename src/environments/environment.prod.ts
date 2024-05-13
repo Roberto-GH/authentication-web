ae6c4d0c934d88609ebd43b5d2ebc387c76ae303
Roberto Londoño <roberthdrums@gmail.com>
2024-05-13 17:37:18 -0500

    new back url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://authenticationapi-env-1.eba-v2esz88g.us-east-1.elasticbeanstalk.com/api/',
-  authenticationURL: 'http://authenticationapi-env-1.eba-v2esz88g.us-east-1.elasticbeanstalk.com/auth/',
-  changePasswordURL: 'http://authenticationapi-env-1.eba-v2esz88g.us-east-1.elasticbeanstalk.com/email-password/'
+  apiURL: 'https://authentication-api.roberto-londono.website/api/',
+  authenticationURL: 'https://authentication-api.roberto-londono.website/auth/',
+  changePasswordURL: 'https://authentication-api.roberto-londono.website/email-password/'
 };
